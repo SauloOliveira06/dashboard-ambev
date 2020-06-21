1881dcb1b6c0575bedfd59eac3a589daa19f2d06
Victor <victor.alvesf7@gmail.com>
2020-06-20 20:20:05 -0400

    removendo endpoint do root service

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ambev-plan.herokuapp.com/mip'
+    baseURL: 'https://ambev-plan.herokuapp.com/'
 // baseURL: 'https://api.tvmaze.com/search/shows?q=star%20wars'
 });
 
