20f05c0134b19244de620a1d6f972886b739bff7
Victor <victor.alvesf7@gmail.com>
2020-06-23 11:13:54 -0400

    removendo arquivos desnecessários

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://ambev-plan.herokuapp.com/'
-// baseURL: 'https://api.tvmaze.com/search/shows?q=star%20wars'
-});
-
-export default api;
