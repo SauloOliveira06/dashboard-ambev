5c2be7a326402580dfd9ac8f4fdee22a4728c2e1
Victor <victor.alvesf7@gmail.com>
2020-06-23 11:19:19 -0400

    componetizando

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
@@ -7,7 +7,7 @@ export default function Footer() {
       <strong>
         Copyright © 2020 <Link to="/"> Arckhtec</Link>
       </strong>
-        
+
       <div className="float-right d-none d-sm-inline-block">
         <b>Versão</b> 1.0.0
       </div>
