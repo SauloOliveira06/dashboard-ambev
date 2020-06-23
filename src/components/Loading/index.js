b866c1978cbe9faa780cfd1fc55bafbbb3873ca6
Victor <victor.alvesf7@gmail.com>
2020-06-23 11:19:03 -0400

    centralizando componente de loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 
 import { Container } from './styles';
 
 function Loading() {
   return (
     <Container>
-      <span>carregando</span>
+      <ReactLoading type="bars" color="#5B81A7" width={50} />
     </Container>
   );
 }
