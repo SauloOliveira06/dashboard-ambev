21290dabd69875808178aca5c171ada39895f5e2
Victor <victor.alvesf7@gmail.com>
2020-06-23 11:21:01 -0400

    estilo global

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  body {
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+`;
