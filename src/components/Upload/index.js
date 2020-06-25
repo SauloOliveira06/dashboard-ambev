ce8657eeea4f0a94e6b4bb5098ba46db35223e4d
Victor <victor.alvesf7@gmail.com>
2020-06-25 00:19:40 -0400

    iniciando componente de upload

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,9 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdCheckCircle, MdInsertDriveFile } from 'react-icons/md';
 
-import { Container } from './styles';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import Dropzone from 'react-dropzone';
+
+import {
+  Container,
+  Content,
+  DropContainer,
+  UploadMessage,
+  FileContainer,
+  FileInfo,
+} from './styles';
 
 function Upload() {
-  return <div />;
+  const [file, setFileName] = useState(null);
+
+  function handleUpload(files) {
+    const fileUpload = files[0];
+
+    if (fileUpload) {
+      setFileName({
+        name: fileUpload.name,
+        size: fileUpload.size,
+      });
+    }
+  }
+
+  function renderMessage(isDragActive, isDragReject) {
+    if (!isDragActive) {
+      return <UploadMessage>Arraste o arquivo aqui...</UploadMessage>;
+    }
+
+    if (isDragReject) {
+      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>;
+    }
+
+    return <UploadMessage type="success">Solte o arquivo aqui</UploadMessage>;
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Dropzone accept="image/*" onDropAccepted={handleUpload}>
+          {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+            <DropContainer
+              {...getRootProps()}
+              isDragActive={isDragActive}
+              isDragReject={isDragReject}
+            >
+              <input {...getInputProps()} />
+              {renderMessage(isDragActive, isDragReject)}
+            </DropContainer>
+          )}
+        </Dropzone>
+        {file !== null && (
+          <FileContainer>
+            <li>
+              <FileInfo>
+                <MdInsertDriveFile size={36} />
+                <div>
+                  <strong>{file.name}</strong>
+                  <span>
+                    {file.size} <button onClick={() => {}}>Remover</button>
+                  </span>
+                </div>
+              </FileInfo>
+              <div>
+                <CircularProgressbar
+                  styles={{
+                    root: {
+                      width: 24,
+                    },
+                    path: {
+                      stroke: '#78e5d5',
+                    },
+                    text: {
+                      fill: '#fff',
+                      fontSize: '40px',
+                    },
+                  }}
+                  strokeWidth={10}
+                  text={`${60}%`}
+                />
+                <MdCheckCircle size={24} color="#78e5d5" />
+              </div>
+            </li>
+          </FileContainer>
+        )}
+      </Content>
+    </Container>
+  );
 }
 
 export default Upload;
--- src/components/Upload/styles.js
+++ src/components/Upload/styles.js
@@ -1,3 +1,93 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+const dragActive = css`
+  border-color: #78e5d5;
+`;
+
+const dragReject = css`
+  border-color: #e57878;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  height: 80vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 40vh;
+  padding: 20px;
+  background: #343a40;
+  border-radius: 4px;
+  color: white;
+`;
+
+export const DropContainer = styled.div.attrs({
+  className: 'dropzone',
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px dashed #ddd;
+  border-radius: 4px;
+  cursor: pointer;
+
+  transition: height 0.4s ease;
+
+  ${(props) => props.isDragActive && dragActive};
+  ${(props) => props.isDragReject && dragReject};
+`;
+
+const messageColors = {
+  default: '#fff',
+  error: '#e57878',
+  success: '#78e5d5',
+};
+
+export const UploadMessage = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => messageColors[props.type || 'default']};
+  padding: 15px 0;
+`;
+
+export const FileContainer = styled.ul`
+  margin-top: 20px;
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-grow: 1;
+  }
+`;
+
+export const FileInfo = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      max-width: 20vh;
+    }
+
+    span {
+      font-size: 12px;
+      margin-top: 5px;
+
+      button {
+        border: 0;
+        background: transparent;
+        color: #e57878;
+        margin-left: 5px;
+      }
+    }
+  }
+`;
