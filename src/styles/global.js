f83d330395612ecf4a7bc1a24d9dac99d8463da5
Victor <victor.alvesf7@gmail.com>
2020-06-25 00:21:53 -0400

    adicionando estilos globais

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,15 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
 
+  p, ul, li, span, button {
+    padding: 0;
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+
   body {
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
